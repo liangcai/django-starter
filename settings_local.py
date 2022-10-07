5ff398ee3a2ee7e8b76a854aa09b011d39a7f984
cail <cail1844@gmail.com>
2022-10-08 07:56:42 +0800

    add jupyter start scripts update dev static & media settings

--- settings_local.py
+++ settings_local.py
@@ -10,6 +10,15 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS += ['django_extensions']
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+
+
 # for jupyter
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
